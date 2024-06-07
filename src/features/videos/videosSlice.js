b31a120a8635030acaff1a58d7477d6e2ac5a9e5
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-06-08 03:51:01 +0600

    videos fetch from server

---
+++ src/features/videos/videosSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import getVideos from "./videosAPI";
+
+
+export const initialState = {
+	isLoading: true,
+	videos: [],
+	isError: false,
+	error: "",
+};
+
+// async thunk function
+export const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
+	const videos = await getVideos();
+	return videos;
+});
+
+// slice for videos
+const videoSlice = createSlice({
+	name: "videos",
+	initialState,
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchVideos.pending, (state) => {
+				state.isError = false;
+				state.isLoading = true;
+			})
+			.addCase(fetchVideos.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.videos = action.payload;
+			})
+			.addCase(fetchVideos.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.videos = [];
+				state.error = action.error?.message;
+			});
+	},
+});
+
+export default videoSlice.reducer;
